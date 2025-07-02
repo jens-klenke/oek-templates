5ae7393e8fa77519d479452fe9cceed9e8f1a0d8
vineet5-data <vineetdairashri5@gmail.com>
2025-07-02 11:40:44 +0200

    xaringan folder problem

--- Resources/quarto_slides/RuhrMetrics_05_2025/assets/slide-numbers.js
+++
@@ -1,8 +0,0 @@
-Reveal.on('slidechanged', function(event) {
-  const totalSlides = Reveal.getTotalSlides();
-  const currentSlide = event.indexh + 1;
-  const footer = document.querySelector('.reveal .footer');
-  if (footer) {
-    footer.innerHTML = `EFEMOD | Slide ${currentSlide} of ${totalSlides}`;
-  }
-});
